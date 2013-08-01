1b164405be5fc8747867b1399f71b2f37ea16606
ricardogsilva <ricardo.garcia.silva@gmail.com>
2013-08-01 01:01:29 +0100

    adding conefor_cli options

--- sextanteconeforprocessor.py
+++ sextanteconeforprocessor.py
@@ -5,11 +5,14 @@ from sextante.core.SextanteConfig import SextanteConfig
 from sextante.parameters.ParameterFile import ParameterFile
 from sextante.parameters.ParameterBoolean import ParameterBoolean
 
-class ConeforProcessor(GeoAlgorithm):
+class ConeforProcessorBase(GeoAlgorithm):
     NAME = 'Conefor test'
     GROUP = 'Conefor'
     INPUT_NODES_FILE = 'INPUT_NODES_FILE'
     INPUT_CONNECTIONS_FILE = 'INPUT_CONNECTIONS_FILE'
+    _type_of_connection_file = 'dist'
+    _connections = 'all'
+    _precision = 'double'
 
     def defineCharacteristics(self):
         self.name = self.NAME
@@ -28,26 +31,57 @@ class ConeforProcessor(GeoAlgorithm):
         print('stdout: %s' % stdout)
         print('stderr: %s' % stderr)
 
-    def _run_conefor(self, command):
+    def _run_conefor(self, nodes_file_path, connections_file_path,
+                     threshold_direct_links=0, binary_indexes=[],
+                     probability_indexes=[],
+                     only_overall=False, write_component_file=False, 
+                     write_links_file=False,
+                     write_dispersal_probabilities_file=False,
+                     write_maximum_probabilities_file=False,
+                     land_area=None):
         '''
         Run Conefor and return the output
         '''
 
         if SextanteConfig.getSetting(self.provider.RUN_THROUGH_WINE):
-            #print('will run through wine')
             command_list = ['wine']
         else:
-            #print('will run without wine')
             command_list = []
-        conefor_executable = SextanteConfig.getSetting(self.provider.CONEFOR_EXECUTABLE_PATH)
-        command_list = command_list + [conefor_executable] + command.split()
+        conefor_executable = SextanteConfig.getSetting(
+            self.provider.CONEFOR_EXECUTABLE_PATH)
+        command_list += [
+            conefor_executable,
+            '-nodeFile', nodes_file_path,
+            '-conFile', connections_file_path,
+            '-t', self._type_of_connection_file,
+            self._connections,
+        ]
+        if any(binary_indexes):
+            command_list += ['-confAdj', threshold_direct_links]
+            if 'BCIIC' in binary_indexes and 'IIC' not in binary_indexes:
+                binary_indexes.append('IIC')
+            for index in binary_indexes:
+                command_list.append('-%s' % index)
+        if any(probability_indexes):
+            command_list += ['-confProb']
+        command_list.append('-%s' % self._precision)
+        if write_component_file and 'NC' in binary_indexes:
+            command_list.append('-wcomp')
+        if write_links_file and any(binary_indexes):
+            command_list.append('-wlinks')
+        if write_dispersal_probabilities_file and any(probability_indexes):
+            command_list.append('-wprobdir')
+        if write_maximum_probabilities_file and 'PC' in probability_indexes:
+            command_list.append('-wprobmax')
+        if land_area is not None:
+            command_list += ['-landArea', landArea]
         print('command_list: %s' % command_list)
         process = Popen(command_list, stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
         return process.returncode, stdout, stderr
 
 
-class ConeforBinaryIndicesProcessor(ConeforProcessor):
+class ConeforBinaryIndicesProcessor(ConeforProcessorBase):
     GROUP = 'Polygons'
     NAME = 'Conefor - Binary indices [%s]' % GROUP
     NL_INDEX = 'NL_INDEX'
@@ -55,13 +89,10 @@ class ConeforBinaryIndicesProcessor(ConeforProcessor):
     NC_INDEX = 'NC_INDEX'
 
     def defineCharacteristics(self):
-        ConeforProcessor.defineCharacteristics(self)
+        ConeforProcessorBase.defineCharacteristics(self)
         self.addParameter(ParameterBoolean(self.NL_INDEX,
                           'NL index', default=False))
         self.addParameter(ParameterBoolean(self.BC_INDEX,
                           'BC index', default=False))
         self.addParameter(ParameterBoolean(self.NC_INDEX,
                           'NC index', default=False))
-
-
-
