f5ee049246de193f98aa7fc67a2284e0846076a6
ricardogsilva <ricardo.garcia.silva@gmail.com>
2013-09-10 16:12:49 +0100

    changes Sextante to Processing

--- coneforinputs.py
+++ coneforinputs.py
@@ -12,8 +12,8 @@ from qgis.core import *
 from coneforinputsprocessor import InputsProcessor
 from conefordialog import ConeforDialog
 
-from sextante.core.Sextante import Sextante
-from sextanteconeforprovider import SextanteConeforProvider
+from processing.core.Processing import Processing
+from processingconeforprovider import ProcessingConeforProvider
 
 
 class NoFeaturesToProcessError(Exception):
@@ -29,10 +29,10 @@ class ConeforProcessor(object):
         self.registry = QgsMapLayerRegistry.instance()
         project_crs = self.iface.mapCanvas().mapRenderer().destinationCrs()
         self.processor = InputsProcessor(project_crs)
-        self.sextante_provider = SextanteConeforProvider()
+        self.processing_provider = ProcessingConeforProvider()
 
     def initGui(self):
-        Sextante.addProvider(self.sextante_provider)
+        Processing.addProvider(self.processing_provider)
         self.action = QAction(QIcon(':plugins/conefor_dev/icon.png'), 
                               self._plugin_name, self.iface.mainWindow())
         QObject.connect(self.action, SIGNAL('triggered()'), self.run)
@@ -40,7 +40,7 @@ class ConeforProcessor(object):
 
     def unload(self):
         self.iface.removePluginVectorMenu('&Conefor inputs', self.action)
-        Sextante.removeProvider(self.sextante_provider)
+        Processing.removeProvider(self.processing_provider)
 
     def run(self):
         dialog = ConeforDialog(self)
